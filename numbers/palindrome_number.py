6c2b741967108eb632cc4f1c6eabf9d50bc1a0c7
adityaraj-dev1 <aadiraj426@gmail.com>
2026-01-24 20:01:12 +0530

    Add palindrome number check

---
+++ numbers/palindrome_number.py
@@ -0,0 +1,13 @@
+num = 121
+original = num
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num //= 10
+
+if original == rev:
+    print("Palindrome number")
+else:
+    print("Not palindrome")
