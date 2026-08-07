b4c2eb440d68a7fd0a4af58debd816f69ee699ed
Aditya Raj <aadiraj426@gmail.com>
2026-08-07 13:14:21 +0530

    Implement lower bound using binary search

---
+++ Binary Search/lower_bound.py
@@ -0,0 +1,21 @@
+def lower_bound(arr, target):
+    left = 0
+    right = len(arr) - 1
+    ans = len(arr)
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] >= target:
+            ans = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return ans
+
+
+arr = [1, 2, 4, 4, 7, 9]
+target = 5
+
+print(lower_bound(arr, target))
