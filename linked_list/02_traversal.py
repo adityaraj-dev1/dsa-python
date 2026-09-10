65e1cb0c9896d2f756b3d6237785fe3c339c5c30
Aditya Raj <aadiraj426@gmail.com>
2026-09-10 12:18:15 +0530

    Add linked list traversal

---
+++ linked_list/02_traversal.py
@@ -0,0 +1,29 @@
+# Singly Linked List - Traversal
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2
+node2.next = node3
+
+head = node1
+
+
+# Traversal
+current = head
+
+while current is not None:
+    print(current.data)
+    current = current.next
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
